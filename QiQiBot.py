bfef2aba8dc872d24c191e5319d6b9ddfaf3bfb4
ctakayama <cole.takayama@gmail.com>
2021-03-19 01:26:53 -0700

    unsub

--- QiQiBot.py
+++ QiQiBot.py
@@ -1,66 +1,128 @@
 import discord
+import const
 from discord.ext import commands, tasks
 from datetime import datetime
 
 client  = commands.Bot(command_prefix =  '-')
 
 users = []
-
-DAILY_URL = 'https://webstatic-sea.mihoyo.com/ys/event/signin-sea/index.html?act_id=e202102251931481&lang=en-us'
+events = []
 
 @client.event
 async def on_ready():
-    print("This is QiQi's daily reminder list")
+    print("Loading QiQi's daily reminder list")
     msgall.start()
-
-@client.command()
-async def signup(ctx):
-    try:
-        myid = format(ctx.message.author.id)
-        print(myid)
-        target = await client.fetch_user(myid)
-        if(myid not in users):
-            await target.send("QiQi has added you to her daily reminders list so you won't forget important things.")
-            await ctx.channel.send("'{}' has been added to QiQi's daily reminders list.".format(ctx.message.author.mention))
-            users.append(myid)
-        else:
-            await ctx.channel.send("{}, QiQi remembers adding you already.".format(ctx.message.author.mention))
-    except:
-        await ctx.channel.send("QiQi couldn't add you to her daily reminders list")
 
 @client.command()
 async def qiqihelp(ctx):
     myEmbed = discord.Embed(
-        title = "QiQi's Daily Reminder List",
-        description = "A reminder bot to make sure QiQi remembers to do her daily logins. \
-        Her list takes the following commands: ",
+        title = const.HELP_TITLE,
+        description = const.HELP_DESC,
         colour = discord.Colour.from_rgb(40, 233, 239)
     )
 
-    myEmbed.add_field(name ="-signup", value ="Qiqi will add you to her daily mailing list", inline =True)
-    myEmbed.add_field(name ="-qiqihelp", value ="Qiqi will show all the commands for this bot", inline =True)
+    myEmbed.add_field(name =const.HELP_SIGNUP_T, value =const.HELP_SIGNUP_D, inline =True)
+    myEmbed.add_field(name =const.HELP_QIQIHELP_T, value =const.HELP_QIQIHELP_D, inline =True)
+    myEmbed.add_field(name =const.HELP_UNSUB_T, value =const.HELP_UNSUB_D, inline =True)
 
     await ctx.channel.send(embed=myEmbed)
 
+@client.command()
+async def signup(ctx, *ids):
+    if len(ids) > 0:
+        for i in ids:
+            try:
+                un = await client.fetch_user(i)
+                if i not in users:
+                    print("adding: " + i + " username: "+ un.name)
+                    users.append(i)
+                else:
+                    print("skip add: " + i + " username: "+ un.name)
+            except:
+                print("not a valid user id")
+    else:
+        try:
+            myid = format(ctx.message.author.id)
+            target = await client.fetch_user(myid)
+            if(myid not in users):
+                await target.send(const.ADD_MSG)
+                await ctx.channel.send("'{}'".format(ctx.message.author.mention)+const.ADD_MSG_SUCC)
+                users.append(myid)
+            else:
+                await ctx.channel.send("'{}'".format(ctx.message.author.mention)+const.ADD_MSG_ALREADY_ADDED)
+        except:
+            await ctx.channel.send("'{}'".format(ctx.message.author.mention)+const.ADD_MSG_ERR)
 
-@tasks.loop(minutes=60.0)
+@client.command()
+async def unsub(ctx, *ids):
+    if len(ids) > 0:
+        for i in ids:
+            try:
+                un = await client.fetch_user(i)
+                if i in users:
+                    print("removing: " + i + " username: "+ un.name)
+                    users.remove(i)
+                else:
+                    print("skip removal: " + i + " username: "+ un.name)
+            except:
+                print("not a valid user id")
+    else:
+        try:
+            myid = format(ctx.message.author.id)
+            target = await client.fetch_user(myid)
+            users.remove(myid)
+            await ctx.channel.send("'{}'".format(ctx.message.author.mention)+const.UNSUB_MSG)
+        except:
+            await ctx.channel.send("'{}'".format(ctx.message.author.mention)+const.UNSUB_MSG_ERR) 
+
+@client.command()
+async def getusers(ctx, arg=None):
+    res = ""
+    for u in users:
+        if(arg == "id"):
+            res += u+" "
+        else:
+            try:
+                un= await client.fetch_user(u)
+                res += un.name+" "
+            except:
+                print("not a valid user id")
+    myid = format(ctx.message.author.id)
+    target = await client.fetch_user(myid)
+    if len(res)>0:
+        await target.send(res)
+    else:
+        await target.send("no ids in user")
+
+@tasks.loop(seconds=10.0)
 async def msgall():
     print("checking the time")
-    if datetime.now().hour ==12:
+
+    embedMsg = discord.Embed(
+        title = const.DAILY_MSG_TITLE,
+        description = const.DAILY_MSG_DESC,
+        colour = discord.Colour.from_rgb(40, 233, 239)
+    )
+
+    #Ongoing events case
+    for t,u in const.CURR_EVENTS:
+        embedMsg.add_field(name =t, value =u, inline =True)
+
+    #weekly boss case
+    if (datetime.now().weekday()) == 6:
+        embedMsg.add_field(name =const.WEEKLY_MSG_BOSS_T, value =const.WEEKLY_MSG_BOSS_D, inline =True)
+
+    embedMsg.set_footer(text = const.DAILY_MSG_FOOT)
+
+    #server is 5 hours ahead pst
+    if datetime.now().hour == 17:
         print("correct time")
         for u in users:
-                print("attempting to msg" + u)
+                print("attempting to msg " + u)
                 try:
                     target = await client.fetch_user(u)
-                    await target.send("QiQi wants to remind you to log in for daily rewards: " + DAILY_URL)
+                    await target.send(embed = embedMsg)
                 except:
-                    await ctx.channel.send("QiQi couldn't send to this user: " + u)
+                    print("QiQi couldn't send to this user: " + u)
 
-
-
-
-
-
-
-client.run('ODIxOTI4NDY3ODMzNjgzOTc4.YFK2iQ.5QYYxSdDiemsA3VRNasnP1frN1Y')
-
+client.run(const.TOKEN)
---
+++ const.py
@@ -0,0 +1,31 @@
+DAILY_URL = 'https://webstatic-sea.mihoyo.com/ys/event/signin-sea/index.html?act_id=e202102251931481&lang=en-us'
+WANMIN_URL = 'https://webstatic-sea.mihoyo.com/ys/event/e20210316cooking-sea/index.html?lang=en-us?utm_source=hoyolab&utm_medium=post'
+
+CURR_EVENTS = [('Daily Login', DAILY_URL), ('Wanmin Web Event', WANMIN_URL)]
+
+HELP_TITLE = "QiQi's Daily Reminder List"
+HELP_DESC = "A reminder bot to make sure QiQi remembers to do her daily logins. \
+            Her list takes the following commands: "
+HELP_SIGNUP_T = "-signup"
+HELP_SIGNUP_D = "Add you to QiQi's daily mailing list"
+HELP_QIQIHELP_T= "-qiqihelp"
+HELP_QIQIHELP_D= "Shows all the commands for this bot"
+HELP_UNSUB_T = "-unsub"
+HELP_UNSUB_D = "Removes you from QiQi's mailing list"
+
+ADD_MSG = "QiQi added you to her daily reminders list so you won't forget important things."
+ADD_MSG_SUCC = " has been added to QiQi's daily reminders list."
+ADD_MSG_ALREADY_ADDED = ", QiQi remembers adding you already."
+ADD_MSG_ERR = ", QiQi couldn't add you to her daily reminders list."
+
+DAILY_MSG_TITLE = "QiQi's Daily Reminder List"
+DAILY_MSG_DESC = "Hello there. Here is your daily reminder for the following events:"
+DAILY_MSG_FOOT ='use -unsub to stop recieving messages'
+
+WEEKLY_MSG_BOSS_T = "Weekly Bosses Reset Tomorrow"
+WEEKLY_MSG_BOSS_D = "Costs 60*3 = 180 resin to unlock all rewards"
+
+UNSUB_MSG = ", QiQi removed you from her mailing list." 
+UNSUB_MSG_ERR = ", QiQi couldn't find you in her mailing list."
+
+TOKEN = 'ODIxOTI4NDY3ODMzNjgzOTc4.YFK2iQ.5QYYxSdDiemsA3VRNasnP1frN1Y'
